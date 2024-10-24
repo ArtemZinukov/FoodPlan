d3066887932584996976b8d07c6cad99cdd4700a
Aleksey525 <alex_tolchin@mail.ru>
2024-10-24 16:10:47 +0300

    Подключил статику и настроил шаблоны

--- foodplan/urls.py
+++ foodplan/urls.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 
+from recipes import views
+
+
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('auth/', views.auth, name='auth'),
+    path('lk/', views.lk, name='lk'),
+    path('order/', views.order, name='order'),
+    path('registration/', views.registration, name='registration'),
+    path('card1/', views.card1, name='card1'),
+    path('card2/', views.card2, name='card2'),
+    path('card3/', views.card3, name='card3'),
+
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def auth(request):
+    return render(request, 'auth.html')
+
+
+def lk(request):
+    return render(request, 'lk.html')
+
+
+def registration(request):
+    return render(request, 'registration.html')
+
+
+def order(request):
+    return render(request, 'order.html')
+
+
+def card1(request):
+    return render(request, 'card1.html')
+
+
+def card2(request):
+    return render(request, 'card2.html')
+
+
+def card3(request):
+    return render(request, 'card3.html')
+
+
