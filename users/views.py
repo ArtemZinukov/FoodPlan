aa92afe325239078edc59311a7bc10b8c34d2518
Shishkin PRO <viktorshish@gmail.com>
2024-10-27 11:09:44 +0300

    Добавил регистрацию

--- users/views.py
+++ users/views.py
@@ -2,10 +2,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
 
-from users.forms import LoginUserForm
+from users.forms import LoginUserForm, RegisterUserForm
 
 
 class LoginUser(LoginView):
@@ -15,3 +15,18 @@ class LoginUser(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('index')
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'users/registration.html', {'form': form})
+    else:
+        form = RegisterUserForm()
+        return render(request, 'users/registration.html', {'form': form})
